4ca6aabb08a53d2c82ea42456889e16425506734
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2025-07-14 11:23:22 +0500

    added phi and spf

--- Math/Modulararithmetic.cpp
+++ Math/Modulararithmetic.cpp
@@ -139,6 +139,14 @@ ll chinese_remainder_theorem(const vector<ll>& remainders, const vector<ll>& mod
     
     return x;
 }
+const int LIM = 5000000;
+int phi[LIM];
+
+void calculatePhi() {
+	rep(i,0,LIM) phi[i] = i&1 ? i : i/2;
+	for (int i = 3; i < LIM; i += 2) if(phi[i] == i)
+		for (int j = i; j < LIM; j += i) phi[j] -= phi[j] / i;
+}
 
 int main() {
     cout << fixed << setprecision(6);
